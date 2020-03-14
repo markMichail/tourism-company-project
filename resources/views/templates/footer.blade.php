42d095b9b247efa2e049de4eeb44f8945480dc38
Mark <mark.refaat.ramzy@gmail.com>
2020-03-14 03:05:11 +0200

    Create header,  main, footer blades

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<!-- <footer class="page-footer font-small blue pt-4">
+
+    <div class="footer-copyright text-center py-3">© 2020 Copyright: Mark Refaat
+        <a href="markrefaat.epizy.com"> markrefaat.com</a>
+    </div>
+
+</footer> -->
+
+<!-- Footer -->
