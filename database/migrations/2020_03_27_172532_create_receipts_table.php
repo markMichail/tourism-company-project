3626620801fe39f794f6015b4017efcd97e4a6c9
Mohamed-Abdelhamed <Mohamed-Abdelhamed@users.noreply.github.com>
2020-03-27 20:10:09 +0200

    model receipts table

---
+++ database/migrations/2020_03_27_172532_create_receipts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('receipt_date');
+            $table->integer('employee_id');
+            $table->boolean('paid');
+            $table->string('type');
+            $table->string('description');
+            $table->integer('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
