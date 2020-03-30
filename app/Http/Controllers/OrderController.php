fce12e5acd56c5663a165ae1bded302a88c2b2c1
fady <fadybassel1@gmail.com>
2020-03-31 01:37:20 +0200

    added authentication to controllers (have to be logged in)

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,11 +7,20 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+
+
+  public function __construct()
+  {
+      $this->middleware('auth');
+}
   /**
   * Display a listing of the resource.
   *
   * @return \Illuminate\Http\Response
   */
+
+ 
+
   public function index()
   {
     $orders = Order::all();
