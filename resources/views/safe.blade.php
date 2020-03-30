b76386bdb2a9dcaa4afc787b7b2695215ed4543e
fady <fadybassel1@gmail.com>
2020-03-31 00:21:30 +0200

    changes in frontend

--- resources/views/safe.blade.php
+++ resources/views/safe.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
+
+
 @section('content')
 
 
@@ -42,7 +44,7 @@
         <tfoot>
           <tr>
             <td colspan="7" class="col-12">
-              <input type="button" class="btn btn-primary" name="" value="Add new expense" data-toggle="modal" data-target="#modaladdnewexpense">
+              <input type="button" class="btn btn-primary" onclick="modal('Expense')" id="Expense" value="Add new expense" data-toggle="modal" data-target="#modaladdnewexpense">
             </td>
           </tr>
         </tfoot>
@@ -83,7 +85,7 @@
         <tfoot>
           <tr>
             <td colspan="7" class="col-12">
-              <input type="button" class="btn btn-primary" name="" value="Add new revenue" data-toggle="modal" data-target="#modaladdnewrevenue">
+              <input type="button" class="btn btn-primary" onclick="modal('Revenue')" name="" value="Add new revenue" data-toggle="modal" data-target="#modaladdnewexpense">
             </td>
           </tr>
         </tfoot>
@@ -107,26 +109,39 @@
           {{ csrf_field() }}
           <div class="form-group">
             <label for="formGroupExampleInput2">Destination Name</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Destination Name" name="destinationname">
+            <input type="text" class="form-control @error('destinationname') is-invalid @enderror"  value="{{ old('destinationname') }}" id="formGroupExampleInput2" placeholder="Destination Name" name="destinationname">
+            @error('destinationname')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Price</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Price" name="price">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="formGroupExampleInput2" placeholder="Price" name="price">
+            @error('price')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Description</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Description" name="description">
+            <input type="text" class="form-control @error('description') is-invalid @enderror" id="formGroupExampleInput2" placeholder="Description" name="description">
+            @error('description')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Date</label>
-            <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="Date" name="date">
+            <input type="date" class="form-control @error('description') is-invalid @enderror" id="formGroupExampleInput2" placeholder="Date" name="date">
+            @error('date')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
           </div>
-          <div class="form-group d-none">
+          <div class="form-group hidden" style="display:none">
             <label for="formGroupExampleInput2">Type</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Type" name="type" value="Expense">
+            <input type="text" class="form-control"   placeholder="Type" id="type" value=""  name="type">
           </div>
           <div class="modal-footer d-flex justify-content-center">
             <button class="btn btn-info btn-block my-4" type="submit">Add</button>
+          
           </div>
         </form>
       </div>
@@ -134,7 +149,7 @@
   </div>
 </div>
 
-<div class="modal fade" id="modaladdnewrevenue" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+{{-- <div class="modal fade" id="modaladdnewrevenue" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
   aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -174,5 +189,16 @@
       </div>
     </div>
   </div>
-</div>
+</div> --}}
+<script>
+  function modal(button){
+    document.getElementById("type").value=button;
+    
+    
+    }
+    @if (count($errors) > 0)
+    $('#modaladdnewexpense').modal('show');
+    @endif
+  </script>
 @endsection
+
