dc5ad4ac5d33a96b479889bfe35f3b683d6665c2
mark <mark.refaat.ramzy@gmail.com>
2020-04-29 17:23:31 +0200

    edit profile / fix 1

--- resources/views/customers/editcustomer.blade.php
+++ resources/views/customers/editcustomer.blade.php
@@ -2,22 +2,22 @@
 @section('content')
 
 <form class="border border-light p-5" action="{{route('update',$customer->id)}}" method="POST" >
-
+    @csrf 
     <p class="h4 mb-4 text-center">Edit Customer</p>
 
     <div class="form-group">
         <label for="formGroupExampleInput">Customer Name</label>
-        <input type="text" class="form-control" id="formGroupExampleInput" value="{{$customer->name}}">
+        <input name="name" type="text" class="form-control" id="formGroupExampleInput" value="{{$customer->name}}">
       </div>
 
       <div class="form-group">
         <label for="formGroupExampleInput2">Phone Number</label>
-        <input type="text" class="form-control" id="formGroupExampleInput2" value="{{$customer->phone}}"
+        <input name="phone" type="text" class="form-control" id="formGroupExampleInput2" value="{{$customer->phone}}">
       </div>
 
       <div class="form-group">
         <label for="formGroupExampleInput2">Email</label>
-        <input type="text" class="form-control" id="formGroupExampleInput2" value="{{$customer->email}}"
+        <input name="email" type="text" class="form-control" id="formGroupExampleInput2" value="{{$customer->email}}">
       </div>
 
     <button class="btn btn-info btn-block my-4" type="submit">Edit</button>
