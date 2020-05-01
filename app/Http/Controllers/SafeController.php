705620d09047ceaa3006871f05628b818433448c
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2020-05-01 05:45:05 +0200

    given admin access to safe

--- app/Http/Controllers/SafeController.php
+++ app/Http/Controllers/SafeController.php
@@ -14,7 +14,9 @@ class SafeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-  }
+        $this->middleware('role:admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
