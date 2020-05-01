148854f416690d19eea6ed5619ce416a1d0b71eb
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2020-05-01 03:11:45 +0200

    rename

--- resources/views/safe.blade.php
+++ resources/views/safe.blade.php
@@ -102,7 +102,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Add Expense</h4>
+        <h4 class="modal-title w-100 font-weight-bold">Add New</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
