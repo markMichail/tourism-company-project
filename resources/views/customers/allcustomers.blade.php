d4ddc89aab478ce082093db3fe7f5676cd398b5f
Mark <mark.refaat.ramzy@gmail.com>
2020-05-02 02:28:04 +0200

    hotfix / add customer

--- resources/views/customers/allcustomers.blade.php
+++ resources/views/customers/allcustomers.blade.php
@@ -6,112 +6,56 @@
 <!-- Editable table -->
 <div style="overflow: hidden;">
 
-@include('customers.sidenav')
+  @include('customers.sidenav')
 
-<div class="card"  style="float:left; width:80%;">
-  <h3 class="card-header text-center font-weight-bold text-uppercase py-4">All Customers table</h3>
-  <div class="card-body">
-    <div id="table" class="table-editable">
+  <div class="card" style="float:left; width:80%;">
+    <h3 class="card-header text-center font-weight-bold text-uppercase py-4">All Customers table</h3>
+    <div class="card-body">
+      <div id="table">
 
-      <table id="dtBasicExample" class="table table-bordered table-responsive-lg table-striped text-center">
-        <thead>
-          <tr>
-            <th class="text-center">Person Name</th>
-            <th class="text-center">Credit</th>
-            <th style="" class="text-center">View</th>
-            <th style="" class="text-center">Edit</th>
-            <th style="" class="text-center">Remove</th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse ($customers as $customer )
-          <tr>
-            <td class="pt-3-half" contenteditable="true">{{$customer->name}}</td>
-            <td class="pt-3-half" contenteditable="true">{{$customer->totalcredit}}</td>
-            <td class="table-view">
-            <a href="customers/{{ $customer->id}}"   <button type="button" class="btn btn-info btn-rounded btn-sm my-0">View</button>
-            </a></td>
-            <td class="table-view">
-            <a href="edit/{{ $customer->id}}"   <button type="button" class="btn btn-info btn-rounded btn-sm my-0">Edit</button>
-            </a></td>
-            <td class="table-remove">
-              <button type="button" class="btn btn-danger px-3"><i class="fas fa-trash" aria-hidden="true"></i></button>
-            </td>
-          </tr>
-          @empty
+        <table id="dtBasicExample" class="table table-bordered table-responsive-lg table-striped text-center">
+          <thead>
+            <tr>
+              <th class="text-center">Person Name</th>
+              <th class="text-center">Credit</th>
+              <th style="" class="text-center">View</th>
+              <th style="" class="text-center">Edit</th>
+              <th style="" class="text-center">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            @forelse ($customers as $customer )
+            <tr>
+              <td class="pt-3-half">{{$customer->name}}</td>
+              <td class="pt-3-half">{{$customer->totalcredit}}</td>
+              <td class="table-view">
+                <a href="customers/{{ $customer->id}}" <button type="button"
+                  class="btn btn-info btn-rounded btn-sm my-0">View</button>
+                </a></td>
+              <td class="table-view">
+                <a href="edit/{{ $customer->id}}" <button type="button"
+                  class="btn btn-info btn-rounded btn-sm my-0">Edit</button>
+                </a></td>
+              <td class="table-remove">
+                <button type="button" class="btn btn-danger px-3"><i class="fas fa-trash"
+                    aria-hidden="true"></i></button>
+              </td>
+            </tr>
+            @empty
             <th colspan="4" class="text-center">No customers to show</th>
-          @endforelse
-        </tbody>
-      </table>
+            @endforelse
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
-</div>
 
 
 </div>
-
-<div class="modal fade" id="modaladdnew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Add customer</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body mx-3">
-
-
-<form action="/addcustomer" class="border border-light p-5">
-
-
-  <div class="form-group">
-      <label for="formGroupExampleInput">Customer Name</label>
-      <input type="text" name="name" class="form-control @error('email') is-invalid @enderror" id="formGroupExampleInput" value="{{old('name')}}" placeholder="customer's name">
-    </div>
-    @error('name')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <div class="form-group">
-      <label for="formGroupExampleInput2">Phone Number</label>
-      <input type="text" name="phone" class="form-control @error('email') is-invalid @enderror" value="{{old('phone')}}" id="formGroupExampleInput2"
-      placeholder="Phone number">
-    </div>
-    @error('phone')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <div class="form-group">
-      <label for="formGroupExampleInput2">Email</label>
-      <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" value="{{old('email')}}" id="formGroupExampleInput2"
-      placeholder="customer's email">
-    </div>
-    @error('email')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-
-
-    <div class="modal-footer d-flex justify-content-center">
-  <button class="btn btn-info btn-block my-4" type="submit">Add</button>
-</div>
-
-</form>
-      </div>
-
-
-
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
+@include('customers.addcustomer')
 
 <script>
-window.onload = function () {
+  window.onload = function () {
   $(document).ready(function () {
     $('#dtBasicExample').DataTable(
       {
--- resources/views/customers/ongoingpayments.blade.php
+++ resources/views/customers/ongoingpayments.blade.php
@@ -6,59 +6,42 @@
 <!-- Editable table -->
 <div style="overflow: hidden;">
 
-@include('customers.sidenav')
+  @include('customers.sidenav')
 
-<div class="card"  style="float:left; width:80%;">
-  <h3 class="card-header text-center font-weight-bold text-uppercase py-4">Ongoing payments</h3>
-  <div class="card-body">
-    <div id="table" class="table-editable">
-      <table id="dtBasicExample" class="table table-bordered table-responsive-lg table-striped text-center">
-        <thead>
-          <tr>
-            <th class="text-center">Person Name</th>
-            <th class="text-center">Date</th>
-            <th class="text-center">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse ($ongoingOrders as $ongoingOrder )
-          <tr>
-            <td class="pt-3-half">{{$ongoingOrder->customer->name}}</td>
-            <td class="pt-3-half">{{$ongoingOrder->date}}</td>
-            <td class="table-view">
-            <a href="/order/{{$ongoingOrder->id}}" class="btn btn-info btn-rounded btn-sm my-0">View</a>
-            </td>
-          </tr>
-          @empty
+  <div class="card" style="float:left; width:80%;">
+    <h3 class="card-header text-center font-weight-bold text-uppercase py-4">Ongoing payments</h3>
+    <div class="card-body">
+      <div id="table" class="table-editable">
+        <table id="dtBasicExample" class="table table-bordered table-responsive-lg table-striped text-center">
+          <thead>
+            <tr>
+              <th class="text-center">Person Name</th>
+              <th class="text-center">Date</th>
+              <th class="text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            @forelse ($ongoingOrders as $ongoingOrder )
+            <tr>
+              <td class="pt-3-half">{{$ongoingOrder->customer->name}}</td>
+              <td class="pt-3-half">{{$ongoingOrder->date}}</td>
+              <td class="table-view">
+                <a href="/order/{{$ongoingOrder->id}}" class="btn btn-info btn-rounded btn-sm my-0">View</a>
+              </td>
+            </tr>
+            @empty
             <th colspan="4" class="text-center">No customers to show</th>
-          @endforelse
-        </tbody>
-      </table>
+            @endforelse
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
-</div>
 
 
 </div>
 
-<div class="modal fade" id="modaladdnew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Add customer</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body mx-3">
-      </div>
-
-
-
-    </div>
-  </div>
-</div>
+@include('customers.addcustomer')
 
 
 
@@ -67,7 +50,7 @@
 
 
 <script>
-window.onload = function () {
+  window.onload = function () {
   $(document).ready(function () {
     $('#dtBasicExample').DataTable(
       {
