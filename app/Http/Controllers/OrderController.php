9949ba16ef71900cde34e65df88312b52ab484f8
Mark <mark.refaat.ramzy@gmail.com>
2020-05-04 14:37:03 +0200

    change cash customer to index 1

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,39 +14,39 @@ class OrderController extends Controller
 
   public function __construct()
   {
-      $this->middleware('auth');
-}
+    $this->middleware('auth');
+  }
   /**
-  * Display a listing of the resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
 
- 
+
 
   public function index()
   {
-    $orders = Order::OrderBy('id','desc')->with('customer')->get();
+    $orders = Order::OrderBy('id', 'desc')->with('customer')->get();
     return view("orders.allorders", compact('orders'));
   }
 
   /**
-  * Show the form for creating a new resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
   public function create()
   {
-    $customers=Customer::all();
-    return view('orders.create',compact('customers'));
+    $customers = Customer::all();
+    return view('orders.create', compact('customers'));
   }
 
   /**
-  * Store a newly created resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @return \Illuminate\Http\Response
-  */
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
   public function store(Request $request)
   {
     $request->validate([
@@ -57,100 +57,101 @@ class OrderController extends Controller
     if ($request->session()->has('order')) {
 
       $request->session()->forget(['order', 'tickets']);
-      }
-    
-      if($request->payment=='cash'){
-        $request->customer_id='0';
-      }
-     $order= Order::create($request->all());
-     $request->session()->put('order',$order);
-     return redirect()->route('orderticketcreate',[$order,$status=0]);
+    }
+
+    if ($request->payment == 'cash') {
+      $request->customer_id = '1';
+    }
+    $order = Order::create($request->all());
+    $request->session()->put('order', $order);
+    return redirect()->route('orderticketcreate', [$order, $status = 0]);
   }
 
   /**
-  * Display the specified resource.
-  *
-  * @param  \App\Order  $order
-  * @return \Illuminate\Http\Response
-  */
+   * Display the specified resource.
+   *
+   * @param  \App\Order  $order
+   * @return \Illuminate\Http\Response
+   */
   public function show(Order $order)
-  {  
-    
+  {
+
     session()->forget('payment');
-     $orderPaymentInfo=$order->ticketsAmount();
-     $data=$orderPaymentInfo[0];
-     $total=$orderPaymentInfo[1];
-     $payed=$orderPaymentInfo[2];
-    return view('orders.show',compact('data','order','total','payed'));
+    $orderPaymentInfo = $order->ticketsAmount();
+    $data = $orderPaymentInfo[0];
+    $total = $orderPaymentInfo[1];
+    $payed = $orderPaymentInfo[2];
+    return view('orders.show', compact('data', 'order', 'total', 'payed'));
   }
 
-  public function confirmpayment(Order $order ,$total){
-    $order->customer->totalcredit-=$total;
+  public function confirmpayment(Order $order, $total)
+  {
+    $order->customer->totalcredit -= $total;
     $order->customer->save();
-    return redirect()->route('order.show',$order);
+    return redirect()->route('order.show', $order);
   }
-  
+
 
   public function confirmview(Order $order)
   {
-    return view('orders.confirm',compact('order'));
+    return view('orders.confirm', compact('order'));
   }
 
   /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  \App\Order  $order
-  * @return \Illuminate\Http\Response
-  */
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Order  $order
+   * @return \Illuminate\Http\Response
+   */
   public function edit(Order $order)
   {
     //
   }
 
   /**
-  * Update the specified resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  \App\Order  $order
-  * @return \Illuminate\Http\Response
-  */
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Order  $order
+   * @return \Illuminate\Http\Response
+   */
   public function update(Request $request, Order $order)
   {
     //
   }
 
   /**
-  * Remove the specified resource from storage.
-  *
-  * @param  \App\Order  $order
-  * @return \Illuminate\Http\Response
-  */
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Order  $order
+   * @return \Illuminate\Http\Response
+   */
   public function destroy($id)
   {
     Order::find($id)->delete();
-    return redirect()->route('order.index')->with('status','order deleted successfully');
+    return redirect()->route('order.index')->with('status', 'order deleted successfully');
   }
 
-  public function print(Order $order){
-    $total=$order->tickets->sum('sellprice');
-    $tickets=$order->tickets;
-    $pdf = PDF::loadView('fatoora',compact('order','tickets','total'));
+  public function print(Order $order)
+  {
+    $total = $order->tickets->sum('sellprice');
+    $tickets = $order->tickets;
+    $pdf = PDF::loadView('fatoora', compact('order', 'tickets', 'total'));
     return $pdf->stream('invoice.pdf');
-
   }
 
-  public function payAll(Order $order){
-    $orderPaymentInfo=$order->ticketsAmount()[0];
-    
-    $allorder="1";
-    $payments=[];
+  public function payAll(Order $order)
+  {
+    $orderPaymentInfo = $order->ticketsAmount()[0];
+
+    $allorder = "1";
+    $payments = [];
     foreach ($orderPaymentInfo as $ticket) {
-      $id=$ticket[0]->id;
-      if($ticket[1]!=='refunded'){
-      $payments["$id"]=["id" => $ticket[0]->id ,"amount" => $ticket[0]->sellprice-$ticket[1]];
+      $id = $ticket[0]->id;
+      if ($ticket[1] !== 'refunded') {
+        $payments["$id"] = ["id" => $ticket[0]->id, "amount" => $ticket[0]->sellprice - $ticket[1]];
       }
     }
-    return view('orders.payment',compact('payments','order',"allorder"));
+    return view('orders.payment', compact('payments', 'order', "allorder"));
   }
-  
 }
--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -4,35 +4,35 @@
 
 <div class=" container">
     <h2 class="text-center">Add new Order</h2>
-<form method="post" action="{{route('order.store')}}">
-    @csrf
-    <label for="date">Date</label>
-    <input type="date" id="date" name="date" class=" @error('date') is-invalid @enderror form-control mb-4"
-        placeholder="Date">
-    @error('date')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-<input type="hidden" name="employee_id" value="{{auth()->user()->id}}">
-    <label for="payment">Payment type</label>
-    <select onchange="viewcustomers()" class="form-control mb-3" name="payment" id="payment">
-        <option value="cash">Cash</option>
-        <option value="agl">later</option>
-    </select>
+    <form method="post" action="{{route('order.store')}}">
+        @csrf
+        <label for="date">Date</label>
+        <input type="date" id="date" name="date" class=" @error('date') is-invalid @enderror form-control mb-4"
+            placeholder="Date">
+        @error('date')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <input type="hidden" name="employee_id" value="{{auth()->user()->id}}">
+        <label for="payment">Payment type</label>
+        <select onchange="viewcustomers()" class="form-control mb-3" name="payment" id="payment">
+            <option value="cash">Cash</option>
+            <option value="agl">later</option>
+        </select>
 
-    <select style="display:none" class="form-control mb-3" name="customer_id" id="customer">
-        @foreach ($customers as $customer )
-        @if($customer->id !== 0)
-        <option value="{{$customer->id}}">{{$customer->name}}</option>
-        @else
-        <option hidden value="{{$customer->id}}">{{$customer->name}}</option>
-        @endif
-        @endforeach
+        <select style="display:none" class="form-control mb-3" name="customer_id" id="customer">
+            @foreach ($customers as $customer )
+            @if($customer->id !== 1)
+            <option value="{{$customer->id}}">{{$customer->name}}</option>
+            @else
+            <option hidden value="{{$customer->id}}">{{$customer->name}}</option>
+            @endif
+            @endforeach
 
-    </select>
-    <div style="text-align: center">
-    <input  class=" btn btn-primary" value="Go add tickets" type="submit">
-    </div>
-</form>
+        </select>
+        <div style="text-align: center">
+            <input class=" btn btn-primary" value="Go add tickets" type="submit">
+        </div>
+    </form>
 
 </div>
 
