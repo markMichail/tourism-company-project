1b8348edc757a7881da389d8d98fd1f180f74252
Mohamed-Abdelhamed <Mohamed-Abdelhamed@users.noreply.github.com>
2020-05-05 16:42:37 +0200

    cust. contoller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Customer;
 use App\Order;
-<<<<<<< HEAD
 use App\Receipt;
-=======
-use App\Setting;
->>>>>>> 2876e76df6890cf59fedb1f735f0d35d1a1c4fdc
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -44,35 +40,33 @@ class CustomerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function ongoingpayments()
-    {
+    {    //index for showing all customers show() for specifed customer.
 
-        $period = Setting::where('name', 'latePeriod')->get()[0]->value;
         $date = strtotime(date('Y-m-d'));
-        $datebefore = date('Y-m-d', strtotime('-' . $period . ' days', $date));
+        $datebefore15days = date('Y-m-d', strtotime('-15 days', $date));
         $counts = [];
-        $counts['ongoingPaymentsCount'] = Order::where('date', '>=', $datebefore)->count();
-        $counts['latePaymentsCount'] = Order::where('date', '<', $datebefore)->count();
+        $counts['ongoingPaymentsCount'] = Order::where('date', '>=', $datebefore15days)->count();
+        $counts['latePaymentsCount'] = Order::where('date', '<', $datebefore15days)->count();
         $counts['customersCount'] = Customer::count();
 
-        $ongoingOrders = Order::where('date', '>=', $datebefore)->with('customer')->get();
+        $ongoingOrders = Order::where('date', '>=', $datebefore15days)->with('customer')->get();
         return view('customers.ongoingpayments', compact('ongoingOrders', 'counts'));
     }
 
-
     public function latepayments()
-    {
+    {    //index for showing all customers show() for specifed customer.
 
-        $period = Setting::where('name', 'latePeriod')->get()[0]->value;
         $date = strtotime(date('Y-m-d'));
-        $datebefore = date('Y-m-d', strtotime('-' . $period . ' days', $date));
+        $datebefore15days = date('Y-m-d', strtotime('-15 days', $date));
         $counts = [];
-        $counts['ongoingPaymentsCount'] = Order::where('date', '>=', $datebefore)->count();
-        $counts['latePaymentsCount'] = Order::where('date', '<', $datebefore)->count();
+        $counts['ongoingPaymentsCount'] = Order::where('date', '>=', $datebefore15days)->count();
+        $counts['latePaymentsCount'] = Order::where('date', '<', $datebefore15days)->count();
         $counts['customersCount'] = Customer::count();
 
-        $lateOrders = Order::where('date', '<', $datebefore)->with('customer')->get();
+        $lateOrders = Order::where('date', '<', $datebefore15days)->with('customer')->get();
         return view('customers.latepayments', compact('lateOrders', 'counts'));
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -114,17 +108,12 @@ class CustomerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
+    {   
         $customer = Customer::find($id);
-<<<<<<< HEAD
         $order = Order::where('customer_id',$customer->id)->where('status','0')->get();
         $expenses = Receipt::where('receiptable_id',$customer->id)->where('receiptable_type','App\Customer')->where('type','expense')->get();
         $revenues = Receipt::where('receiptable_id',$customer->id)->where('receiptable_type','App\Customer')->where('type','revenue')->get();
         return view('customers.customerprofile', compact('customer','order','expenses','revenues'));
-=======
-        $order = Order::where('customer_id', $customer->id)->where('status', '0')->get();
-        return view('customers.customerprofile', compact('customer', 'order'));
->>>>>>> 2876e76df6890cf59fedb1f735f0d35d1a1c4fdc
     }
 
     public function updatenote($id)
