90eda87038288f212b5508c95c75052d1d626747
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2020-05-06 03:09:04 +0200

    unauthorized will return back to home

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -22,6 +22,6 @@ class CheckRole
                 return $next($request);
             }
         }
-        abort(401, 'This action is unauthorized.');
+        return redirect(route('home'));
     }
 }
