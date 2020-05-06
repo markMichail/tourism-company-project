20d6d08cfe3af5357acd83b3d43bf54e2b598838
Mark <mark.refaat.ramzy@gmail.com>
2020-05-06 02:49:03 +0200

    user roles / hotfix

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Role;
 use Closure;
 
 class CheckRole
@@ -13,11 +14,14 @@ class CheckRole
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $role)
+    public function handle($request, Closure $next, ...$roles)
     {
-        if (! $request->user()->hasRole($role)) {
-            abort(401, 'This action is unauthorized.');
+        $userRole = Role::where('id', $request->user()->privilege)->get()[0]->name;
+        foreach ($roles as $role) {
+            if ($userRole == $role) {
+                return $next($request);
+            }
         }
-        return $next($request);
+        abort(401, 'This action is unauthorized.');
     }
 }
