d47a57b74139cbfa2792bc32c3a886e42a54ac44
fady <fadybassel1@gmail.com>
2020-05-09 00:25:14 +0200

    remove unused functions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -113,28 +113,6 @@ class OrderController extends Controller
     else return redirect()->route("order.index")->with('status','Order already payed');
   }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  \App\Order  $order
-   * @return \Illuminate\Http\Response
-   */
-  public function edit(Order $order)
-  {
-    //
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \App\Order  $order
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, Order $order)
-  {
-    //
-  }
 
   /**
    * Remove the specified resource from storage.
