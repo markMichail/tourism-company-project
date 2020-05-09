48dfd3f458a58d91195f83dba207863a3fd2cae6
Mark <mark.refaat.ramzy@gmail.com>
2020-05-09 18:02:21 +0200

    user roles / hotfix

--- app/Order.php
+++ app/Order.php
@@ -6,39 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-  
-    protected $fillable=['customer_id','employee_id','status','date'];
-    public function tickets(){
-      return $this->hasMany('App\Ticket');
+
+  protected $fillable = ['customer_id', 'employee_id', 'status', 'date'];
+  public function tickets()
+  {
+    return $this->hasMany('App\Ticket');
   }
 
 
-  public function customer(){
+  public function customer()
+  {
     return  $this->belongsTo('App\Customer');
   }
 
-  public function ticketsAmount(){
-    $data=[];
-    $tickets = Ticket::with('receipts')->where('order_id',$this->id)->get();
-    $ordertotal=0;
-    $payed=0;
+  public function ticketsAmount()
+  {
+    $data = [];
+    $tickets = Ticket::with('receipts')->where('order_id', $this->id)->get();
+    $ordertotal = 0;
+    $payed = 0;
     foreach ($tickets as $ticket) {
-      if($ticket->type!='refunded'){
-      $receipts=$ticket->receipts;
-      $ordertotal+=$ticket->sellprice;
-      if($receipts->count()>0){
-        $total=0;
-        foreach ($receipts as $receipt) {
-          if($receipt->type=="revenue"){
-          $total+=$receipt->pivot->amount;
-          $payed+=$receipt->pivot->amount;
+      if ($ticket->type != 'refunded') {
+        $receipts = $ticket->receipts;
+        $ordertotal += $ticket->sellprice;
+        if ($receipts->count() > 0) {
+          $total = 0;
+          foreach ($receipts as $receipt) {
+            if ($receipt->type == "revenue") {
+              $total += $receipt->pivot->amount;
+              $payed += $receipt->pivot->amount;
+            }
           }
-        }   
-        array_push($data,[$ticket,$total]);
-      } else {array_push($data,[$ticket,0]);} 
-    } else {array_push($data,[$ticket,"refunded"]);}
+          array_push($data, [$ticket, $total]);
+        } else {
+          array_push($data, [$ticket, 0]);
+        }
+      } else {
+        array_push($data, [$ticket, "refunded"]);
+      }
+    }
+    return [$data, $ordertotal, $payed];
   }
-    return [$data,$ordertotal,$payed];
-  }
-
 }
--- app/Role.php
+++ app/Role.php
@@ -18,8 +18,6 @@ class Role extends Model
 
     public function users()
     {
-        return $this
-            ->belongsToMany('App\User')
-            ->withTimestamps();
+        return $this->belongsToMany('App\User');
     }
 }
